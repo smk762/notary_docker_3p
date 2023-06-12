462c2679b1bd0aa61469c89a56759b70dad05560
smk762 <smk762@iinet.net.au>
2023-06-12 17:21:16 +0800

    Add CHIPS files

---
+++ chips/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+username=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
+rpcpass=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
+
+mkdir -p ~/.chips
+echo "server=1" > ~/.chips/chips.conf
+echo "listen=1" >> ~/.chips/chips.conf
+echo "daemon=1" >> ~/.chips/chips.conf
+echo "txindex=1" >> ~/.chips/chips.conf
+echo "spentindex=1" >> ~/.chips/chips.conf
+echo "addressindex=1" >> ~/.chips/chips.conf
+echo "p2pport=41887" >> ~/.chips/chips.conf
+echo "rpcport=41888" >> ~/.chips/chips.conf
+echo "rpcuser=${username}" >> ~/.chips/chips.conf
+echo "rpcpassword=${rpcpass}" >> ~/.chips/chips.conf
+echo "rpcbind=127.0.0.1" >> ~/.chips/chips.conf
+echo "rpcallowip=127.0.0.1" >> ~/.chips/chips.conf
+
+# cat ~/.chips/chips.conf
+
+exec "$@"
---
+++ chips/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+chipsd -pubkey=${PUBKEY} -addnode=5.9.253.195
+sleep 20
+ls -la ~/.chips
+tail -f ~/.chips/debug.log
