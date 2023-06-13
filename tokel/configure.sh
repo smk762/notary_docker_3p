0cdc10dee678a558189241f1d80caa40f7ccfa2c
smk762 <smk762@iinet.net.au>
2023-06-14 05:37:13 +0800

    fix tokel conf filename

--- tokel/configure.sh
+++ tokel/configure.sh
@@ -1,18 +1,18 @@
 
 mkdir -p /home/komodian/.komodo/TOKEL
-echo "server=1" > /home/komodian/.komodo/TOKEL/komodo.conf
-echo "listen=1" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "daemon=1" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "txindex=1" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "spentindex=1" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "addressindex=1" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "port=29404" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "rpcport=29405" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "rpcuser=${RPC_USER}" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "rpcpassword=${RPC_PASS}" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "rpcbind=0.0.0.0:29405" >> /home/komodian/.komodo/TOKEL/komodo.conf
-echo "rpcallowip=0.0.0.0/0" >> /home/komodian/.komodo/TOKEL/komodo.conf
+echo "server=1" > /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "listen=1" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "daemon=1" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "txindex=1" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "spentindex=1" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "addressindex=1" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "port=29404" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "rpcport=29405" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "rpcuser=${RPC_USER}" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "rpcpassword=${RPC_PASS}" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "rpcbind=0.0.0.0:29405" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
+echo "rpcallowip=0.0.0.0/0" >> /home/komodian/.komodo/TOKEL/TOKEL.conf
 
 echo "" > /home/komodian/.komodo/TOKEL/debug.log
 
-# cat /home/komodian/.komodo/TOKEL/komodo.conf
+# cat /home/komodian/.komodo/TOKEL/TOKEL.conf
