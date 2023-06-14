7a24fdfe7559f1efeec147adaee412fb735a9384
smk762 <smk762@iinet.net.au>
2023-06-15 06:38:25 +0800

    allow start script to take service as param

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-docker compose up -d && docker compose logs -f --tail 20
+#!/bin/bash
+set -euxo pipefail
+
+if [ -z "$1" ]
+  then
+    docker compose up -d && docker compose logs -f --tail 20
+  else
+    docker compose up $1 -d && docker compose logs $1 -f --tail 20
+fi
+
