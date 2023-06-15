cbb87a9ed48dee382536a544c50970247ce7122f
smk762 <smk762@iinet.net.au>
2023-06-16 04:12:04 +0800

    allow selective builds

--- update_3p.sh
+++ update_3p.sh
@@ -8,5 +8,16 @@ rm docker-compose.yml
 cp docker-compose.template docker-compose.yml
 sed "s/USERNAME/${USER}/gi" -i "docker-compose.yml"
 
-echo "Building docker images..."
-docker compose build
+
+if [ -z "$1" ]
+  then
+    echo "Building docker images..."
+    docker compose build
+    ./start_3p.sh
+  else
+    echo "Building docker images..."
+    docker compose build $1
+    ./start_3p.sh $1
+fi
+
+
