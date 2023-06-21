e78cd1437bbf10a6b3c2e70b84dd66b66aa8c3bf
gcharang <21151592+gcharang@users.noreply.github.com>
2023-06-22 00:28:29 +0530

    3p cli script, better stop script

--- stop_3p.sh
+++ stop_3p.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+source ./service_cli
+
 if [ -z "$1" ]
   then
+    for key in "${!service_cli[@]}"; do
+      ${service_cli[$key]} stop
+    done
     docker compose stop
   else
+    ${service_cli[$i]} stop
     docker compose stop $1
 fi
 
