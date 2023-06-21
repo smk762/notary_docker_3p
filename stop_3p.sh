1d1aca3049358356976305c841774cc62390260f
gcharang <21151592+gcharang@users.noreply.github.com>
2023-06-22 00:32:24 +0530

    correction

--- stop_3p.sh
+++ stop_3p.sh
@@ -4,14 +4,13 @@ set -euxo pipefail
 
 source ./service_cli
 
-if [ -z "$1" ]
-  then
-    for key in "${!service_cli[@]}"; do
-      ${service_cli[$key]} stop
-    done
-    docker compose stop
-  else
-    ${service_cli[$i]} stop
-    docker compose stop $1
+if [ -z "$1" ]; then
+  for key in "${!service_cli[@]}"; do
+    ${service_cli[$key]} stop
+  done
+  docker compose stop
+else
+  ${service_cli[$i]} stop
+  docker compose stop $1
 fi
 
