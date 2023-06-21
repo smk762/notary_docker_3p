e16891555de9bf16c8235629030a9ae681eeacd8
gcharang <21151592+gcharang@users.noreply.github.com>
2023-06-22 00:40:43 +0530

    correction

--- cli_3p.sh
+++ cli_3p.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 source ./service_cli
 
-if [ -z "$2" ]; then
+if [[ -z ${2-} ]]; then
   for key in "${!service_cli[@]}"; do
     ${service_cli[$key]} $1
   done
--- stop_3p.sh
+++ stop_3p.sh
@@ -8,9 +8,11 @@ if [[ -z ${1-} ]]; then
   for key in "${!service_cli[@]}"; do
     ${service_cli[$key]} stop
   done
+  sleep 60
   docker compose stop
 else
   ${service_cli[$1]} stop
+  sleep 60
   docker compose stop $1
 fi
 
