ef5be6fc6e6f91daf05ba1976f3dbd044b951a7b
smk762 <smk762@iinet.net.au>
2023-06-30 00:40:59 +0800

    you're a notary harry

--- configure.py
+++ configure.py
@@ -243,7 +243,7 @@ def get_launch_params(coin):
     if coin == 'KMD':
         launch += " -gen -genproclimit=1 -minrelaytxfee=0.000035 -opretmintxfee=0.004 -notary=.litecoin/litecoin.conf"
     elif coin == 'KMD_3P':
-        launch += " -minrelaytxfee=0.000035 -opretmintxfee=0.004"
+        launch += " -minrelaytxfee=0.000035 -opretmintxfee=0.004 -notary"
     elif coin == 'MCL':
         launch += " -ac_name=MCL -ac_supply=2000000 -ac_cc=2 -addnode=5.189.149.242 -addnode=161.97.146.150 -addnode=149.202.158.145 -addressindex=1 -spentindex=1 -ac_marmara=1 -ac_staked=75 -ac_reward=3000000000 -daemon"
 
