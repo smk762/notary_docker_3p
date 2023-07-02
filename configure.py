74aacdf2332e115ab8f3b760ad9a220ecbc4aa8c
smk762 <smk762@iinet.net.au>
2023-07-02 21:09:20 +0800

    chec for iguana pubkeys

--- configure.py
+++ configure.py
@@ -6,130 +6,7 @@ import secrets
 import json
 import sys
 import os
-
-# Get the list of coins
-
-home = os.path.expanduser('~')
-script_path = os.path.realpath(os.path.dirname(__file__))
-dpow_path = os.path.join(home, "dpow")
-
-with open(f'assetchains.json') as file:
-    assetchains = json.load(file)
-
-coins_main = {
-    "CCL": {
-        "daemon": "komodod",
-        "p2pport": 20848,
-        "rpcport": 20849
-    },
-    "KMD": {
-        "daemon": "komodod",
-        "p2pport": 7770,
-        "rpcport": 7771
-    },
-    "LTC": {
-        "daemon": "litecoind",
-        "p2pport": 9333,
-        "rpcport": 9332
-    },
-    "CLC": {
-        "daemon": "komodod",
-        "p2pport": 20931,
-        "rpcport": 20932
-    },
-    "GLEEC": {
-        "daemon": "komodod",
-        "p2pport": 23225,
-        "rpcport": 23226
-    },
-    "ILN": {
-        "daemon": "komodod",
-        "p2pport": 12985,
-        "rpcport": 12986
-    },
-    "NINJA": {
-        "daemon": "komodod",
-        "p2pport": 8426,
-        "rpcport": 8427
-    },
-    "KOIN": {
-        "daemon": "komodod",
-        "p2pport": 10701,
-        "rpcport": 10702
-    },
-    "PIRATE": {
-        "daemon": "komodod",
-        "p2pport": 45452,
-        "rpcport": 45453
-    },
-    "SUPERNET": {
-        "daemon": "komodod",
-        "p2pport": 11340,
-        "rpcport": 11341
-    },
-    "THC": {
-        "daemon": "komodod",
-        "p2pport": 36789,
-        "rpcport": 36790
-    },
-    "DOC": {
-        "daemon": "komodod",
-        "p2pport": 62415,
-        "rpcport": 62416
-    },
-    "MARTY": {
-        "daemon": "komodod",
-        "p2pport": 52592,
-        "rpcport": 52593
-    }
-}
-
-# Todo: add commit hashes for 3p coins
-coins_3p = {
-    "KMD_3P": {
-        "daemon": "komodod",
-        "p2pport": 8770,
-        "rpcport": 8771
-    },
-    "AYA": {
-        "daemon": "aryacoind",
-        "p2pport": 26001,
-        "rpcport": 9432
-    },
-    "CHIPS": {
-        "daemon": "chipsd",
-        "p2pport": 57776,
-        "rpcport": 57777
-    },
-    "EMC2": {
-        "daemon": "einsteiniumd",
-        "p2pport": 41878,
-        "rpcport": 41879
-    },
-    "MCL": {
-        "daemon": "komodod",
-        "p2pport": 33824,
-        "rpcport": 33825
-    },
-    "MIL": {
-        "daemon": "mild",
-        "p2pport": 41888,
-        "rpcport": 41889
-    },
-    "TOKEL": {
-        "daemon": "tokeld",
-        "p2pport": 29404,
-        "rpcport": 29405
-    },
-    "VRSC": {
-        "daemon": "verusd",
-        "p2pport": 27485,
-        "rpcport": 27486
-    }    
-}
-
-# Only using 3P coins in this repo
-coins = list(coins_3p.keys())
+from const import home, script_path, dpow_path, coins_main, coins_3p, assetchains
 
 
 def format_param(param, value):
@@ -279,6 +156,10 @@ def get_user_pubkey(server='3p'):
         file = f"{dpow_path}/iguana/pubkey_3p.txt"
     else:
         file = f"{dpow_path}/iguana/pubkey.txt"
+    if not os.path.exists(file):
+        print(f"{file} does not exist!")
+        print(f"Please install dPoW, and make sure to create your pubkey.txt and pubkey_3p.txt files in {dpow_path}/iguana!")
+        sys.exit()
     if os.path.exists(file):
         with open(file, 'r') as f:
             for line in f:
@@ -442,6 +323,11 @@ def create_compose_yaml(server='3p'):
                 conf.write('        max-file: "10"\n')
                 conf.write(f'    command: ["/run_{coin}.sh"]\n')
                 conf.write('\n')
+
+# Tests to confirm pubeys set
+get_user_pubkey('main')
+
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
