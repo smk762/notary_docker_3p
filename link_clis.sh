df95803b75cbf9c415c7d541518ed091f33ed63e
smk762 <smk762@iinet.net.au>
2023-09-01 20:51:51 +0800

    update wrappers to include {ticker}-cli

--- link_clis.sh
+++ link_clis.sh
@@ -5,6 +5,7 @@ script_path=$(dirname $(realpath $0))
 # AYA
 rm -f /home/$USER/.aryacoin/aryacoin-cli
 ln -sf ${script_path}/cli_wrappers/aryacoin-cli /home/$USER/.local/bin/aryacoin-cli
+ln -sf ${script_path}/cli_wrappers/aryacoin-cli /home/$USER/.local/bin/aya-cli
 
 # CHIPS
 rm -f /home/$USER/.chips/chips-cli
@@ -12,7 +13,8 @@ ln -sf ${script_path}/cli_wrappers/chips-cli /home/$USER/.local/bin/chips-cli
 
 # EMC2
 rm -f /home/$USER/.einsteinium/einsteinium-cli
-ln -sf ${script_path}/cli_wrappers/einsteinium-cli /home/$USER/.local/bin/einsteinium-cli
+ln -sf ${script_path}/cli_wrappers/emc-cli /home/$USER/.local/bin/emc-cli
+ln -sf ${script_path}/cli_wrappers/emc-cli /home/$USER/.local/bin/einsteinium-cli
 
 # KMD (3P)
 rm -f /home/${USER}/.komodo_3p/komodo_3p-cli
@@ -20,6 +22,7 @@ rm -f /home/${USER}/.komodo_3p/komodo-cli
 rm -f /home/${USER}/.komodo_3p/*-cli
 rm -f /home/${USER}/.komodo/*-cli
 ln -sf ${script_path}/cli_wrappers/komodo_3p-cli /home/$USER/.local/bin/komodo_3p-cli
+ln -sf ${script_path}/cli_wrappers/komodo_3p-cli /home/$USER/.local/bin/kmd_3p-cli
 
 # MCL
 rm -f /home/${USER}/.komodo_3p/MCL/mcl-cli
@@ -33,8 +36,10 @@ ln -sf ${script_path}/cli_wrappers/mil-cli /home/$USER/.local/bin/mil-cli
 rm -f /home/$USER/.komodo_3p/TOKEL/tokel-cli-bin
 rm -f /home/$USER/.komodo_3p/TOKEL/tokel-cli
 ln -sf ${script_path}/cli_wrappers/tokel-cli /home/$USER/.local/bin/tokel-cli
+ln -sf ${script_path}/cli_wrappers/tokel-cli /home/$USER/.local/bin/tkl-cli
 
 # VRSC
 rm -f /home/$USER/.komodo_3p/VRSC/verus-cli-bin
 rm -f /home/$USER/.komodo_3p/VRSC/verus-cli
 ln -sf ${script_path}/cli_wrappers/verus-cli /home/$USER/.local/bin/verus-cli
+ln -sf ${script_path}/cli_wrappers/verus-cli /home/$USER/.local/bin/vrsc-cli
